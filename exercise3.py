fbc50a0c1d05393bf4a6c4c5a18d4b40e36ef0d2
Aki78 <kmorooka78@gmail.com>
2022-08-29 13:25:09 +0300

    done testing, next exercise4.

--- exercise3.py
+++ exercise3.py
@@ -1,54 +1,51 @@
-l = input("how long was zander: ")
-
-if (l < 42)
-    print("It is too short, reliease it back into the lake.")
-else:
-    print("Keep it.")
-
-
-
-c = input("Enter your cabin class: ")
-
-if (c == "LUX"):
-    print("upper-deck cabin with a balcony.")
-
-elif (c == "A"):
-    print("above the car deck, equipped with a window.")
-elif (c == "B"):
-    print("windowless cabin above the car deck.")
-elif (c == "C"):
-    print("windowless cabin below the car deck.")
-else:
-    print("Invalid cabin class.")
-
-
-g,h = input("Enter the gender (f or m) hemoglobin level with spaces: ")
-h = int(h)
-
-if ( g== "f"):
-    if (117 > h):
-        print("hemoglobin level too low.")
-    elif (155 < h):
-        print("hemoglobin level too heigh.")
-    else:
-        print("hemoglobin level normal.")
-elif ( g== "m"):
-    if (134 > h):
-        print("hemoglobin level too low.")
-    elif (167 < h):
-        print("hemoglobin level too heigh.")
-    else:
-        print("hemoglobin level normal.")
-else:
-    print("not a gender")
+#l = input("how long was zander: ")
+#
+#if (l < 42)
+#    print("It is too short, reliease it back into the lake.")
+#else:
+#    print("Keep it.")
+#
+#c = input("Enter your cabin class: ")
+#
+#if (c == "LUX"):
+#    print("upper-deck cabin with a balcony.")
+#
+#elif (c == "A"):
+#    print("above the car deck, equipped with a window.")
+#elif (c == "B"):
+#    print("windowless cabin above the car deck.")
+#elif (c == "C"):
+#    print("windowless cabin below the car deck.")
+#else:
+#    print("Invalid cabin class.")
+#
+#g,h = input("Enter the gender (f or m) hemoglobin level with spaces: ")
+#h = int(h)
+#
+#if ( g== "f"):
+#    if (117 > h):
+#        print("hemoglobin level too low.")
+#    elif (155 < h):
+#        print("hemoglobin level too heigh.")
+#    else:
+#        print("hemoglobin level normal.")
+#elif ( g== "m"):
+#    if (134 > h):
+#        print("hemoglobin level too low.")
+#    elif (167 < h):
+#        print("hemoglobin level too heigh.")
+#    else:
+#        print("hemoglobin level normal.")
+#else:
+#    print("not a gender")
 
 def is_leap_year():
-    y = input("input year" )
+    y = int(input("input year: " ))
     if y % 4 != 0:
-        return ("Not Leap year")
-    if y % 4 == 400:
-        return ("Leap year")
-    if y % 100:
-        return("Not Leap year")
-    return ("Leap Year")
-is_leay_year()
+        return ("Not Leap year: ")
+    if y % 400 == 0:
+        return ("Leap year: ")
+    if y % 100 == 0:
+        return("Not Leap year: ")
+    return ("Leap Year.")
+print(is_leap_year())
