01259b59f030f9e3eff03603a411ce4f06dfb2e0
Aki78 <kmorooka78@gmail.com>
2022-08-29 15:26:34 +0300

    done part 5 exercise 4

--- exercise4.py
+++ exercise4.py
@@ -29,19 +29,45 @@
 #    else:
 #        print("largest number: ", largest, "smallest number: ", smallest)
 #        break
+#
+## 4
+#import random
+#r = random.randint(1,10)
+#while 1:
+#    l = int(input("guess a number between 1-10: "))
+#    if ( r> l):
+#        print("too low")
+#    elif (r < l):
+#        print("too high")
+#    else:
+#        print("correct")
+#        break
 
-# 4
-import random
-r = random.randint(1,10)
-while 1:
-    l = int(input("guess a number between 1-10: "))
-    if ( r> l):
-        print("too low")
-    elif (r < l):
-        print("too high")
-    else:
-        print("correct")
+# 5
+import hashlib
+from getpass import getpass
+
+l = 5
+# user_name = "python"
+# pass_word = "rules"
+while l > -1:
+    h = hashlib.new('sha256')
+    h.update("pythonrules".encode())
+    print(h.hexdigest())
+
+    my_hash = "fd7a4c43ad7c20dbea0dc6dacc12ef6c36c2c382a0111c92f24244690eba65a2"
+    u = input("user name: ")
+    p = getpass("password (chars will not show): ")
+
+    h = hashlib.new('sha256')
+    h.update((u + p).encode())
+
+    if (my_hash == h.hexdigest()):
+        print("access approved")
         break
-
-
+    else:
+        print("access denied", l, "times left")
+        if (l == 0):
+            print("ACCOUNT TERMINATED, please contact the IT department to renew your account. (Phone number: (+1) 045-555-6390.")
+    l -= 1
 
