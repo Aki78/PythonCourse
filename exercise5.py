d4837cc12a596448d6440cdedca85f75f4a57ef5
Aki78 <kmorooka78@gmail.com>
2022-09-01 14:45:59 +0300

    done exercise 5

--- exercise5.py
+++ exercise5.py
@@ -1,24 +1,26 @@
-## exercise 1
-#import random
-#
-#n = int(input("how many times do you want to roll the dice?: "))
-#accum = 0
-#
-#for i in range(n):
-#    accum += random.randint(1,6)
-#
-#print("the sum is: ", accum)
-#
-## exercise 2
-#
-#arr = []
-#while 1:
-#    n = input("how many times do you want to roll the dice?: ")
-#    if (not n.isdigit()):
-#        break
-#    arr.append(int(n))
-#arr.sort(reverse=True)
-#print("the top 5 values are: ", arr[:5])
+# section 1
+import random
+
+n = int(input("how many times do you want to roll the dice?: "))
+accum = 0
+
+for i in range(n):
+    accum += random.randint(1,6)
+
+print("the sum is: ", accum)
+
+# section 2
+
+arr = []
+while 1:
+    n = input("how many times do you want to roll the dice?: ")
+    if (not n.isdigit()):
+        break
+    arr.append(int(n))
+arr.sort(reverse=True)
+print("the top 5 values are: ", arr[:5])
+
+# section 3
 all_prime_numbers = [2,3]
 n = int(input("type in a number to check if its prime: "))
 for i in range(2,n+1):
@@ -34,3 +36,13 @@ if  (n in all_prime_numbers):
     print("The number is a prime")
 else:
     print("The number is not a prime")
+
+# section 4
+arr = []
+for i in range(5):
+    n = input("type in a city name: ")
+    arr.append(n)
+print("Tye cities you typed are:")
+for i in arr:
+    print(i)
+
