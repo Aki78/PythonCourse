6d24cb0b486662424174aa86622bcb570012b769
Aki78 <kmorooka78@gmail.com>
2022-09-05 17:30:27 +0300

    connected to mariadb and printed out airport names

--- exercise7.py
+++ exercise7.py
@@ -22,8 +22,22 @@
 
 
 # 3
+
 import mysql.connector
-yhteys = mysql.connector.connect(
+def getEmployeesByLastName():
+    sql = "SELECT name from airport"
+    # sql += " WHERE Last_name='" + last_name + "'"
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount >0 :
+        for row in result:
+            # print(f"Hello! I'm {row[2]} {row[1]}. My salary is {row[3]} euros per month.")
+            print(row)
+    return
+
+connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
          database='flight_game',
@@ -32,4 +46,4 @@ yhteys = mysql.connector.connect(
          autocommit=True
          )
 
-print(yhteys)
+print(getEmployeesByLastName())
