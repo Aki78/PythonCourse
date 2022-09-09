d05ca12d35646a2a62c295a1320b871f8b05d481
Aki78 <kmorooka78@gmail.com>
2022-09-09 12:50:45 +0300

    done exercise 7.3 find function

--- exercise7.py
+++ exercise7.py
@@ -24,33 +24,7 @@
 # 3
 
 import mysql.connector
-def get_employees_byLast_name():
-    sql = "SELECT name from airport"
-    # sql += " WHERE Last_name='" + last_name + "'"
-    print(sql)
-    cursor = connection.cursor()
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    if cursor.rowcount >0 :
-        for row in result:
-            # print(f"Hello! I'm {row[2]} {row[1]}. My salary is {row[3]} euros per month.")
-            print(row)
-    return
-choose = input("would you like to A: add, F:find or Q:Quit ")
-
-def add_airport(x):
-    print(x)
-
-def find_airport(x):
-    print(x)
-
-while 1:
-    if choose == 'A':
-        add_airport("A")
-    elif choose == 'F':
-        find_airport()
-    else:
-        break
+from colorama import Fore
 
 connection = mysql.connector.connect(
          host='127.0.0.1',
@@ -61,4 +35,43 @@ connection = mysql.connector.connect(
          autocommit=True
          )
 
-#print(get_employees_byLast_name())
+def get_employees_byLast_name():
+    sql = "SELECT name from airport"
+    # sql += " WHERE Last_name='" + last_name + "'"
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            print(row)
+    return
+
+print(get_employees_byLast_name())
+
+def add_airport(x):
+    print(x)
+
+def find_airport_by_icao(icao):
+    sql = 'SELECT name from airport'
+    sql += ' WHERE ident = "' + icao + '"'
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            print("Airport name is: " , Fore.RED, row[0] , Fore.RESET)
+    return
+
+while 1:
+    choose = input("would you like to A: add, F:find or Q:Quit ")
+    if choose == 'A' or  choose == 'a':
+        add_airport()
+    elif choose == 'F' or  choose == 'f':
+        choose = input("Type icao code: ")
+        find_airport_by_icao(choose)
+    else:
+        break
+
+
