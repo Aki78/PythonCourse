c7b3c46d41a64d2c4a00409216f7f731c21dac5f
Aki78 <kmorooka78@gmail.com>
2022-09-10 12:22:13 +0300

    done exercise 11.1

---
+++ exercise11.py
@@ -0,0 +1,30 @@
+#1
+
+class Publication:
+  def __init__(self, name):
+      self.name = name
+
+class Book(Publication):
+    def __init__(self, name, author, page_n):
+        self.author = author
+        self.page_n = page_n
+        super().__init__(name)
+    def print_information(self):
+        print("name:", self.name, "author:", self.author, "page number:", self.page_n)
+
+
+class Magazine(Publication):
+    def __init__(self,name, chief_editor):
+        self.chief_editor = chief_editor
+        super().__init__(name)
+
+    def print_information(self):
+        print("name:", self.name, "chief_editor:", self.chief_editor)
+
+
+
+b = Book("Campartment No. 6", "Rosa Liksom", 192)
+m = Magazine("Donald Duck", "Aki Hyppa")
+
+b.print_information()
+m.print_information()
